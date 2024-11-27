b666b63b393b1c93f2cbec33688715e41c554d1f
espageti <darreeb.lin@gmail.com>
2024-11-26 17:54:19 -0800

    change git ignore'

--- steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
+++ steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
@@ -3,11 +3,13 @@ package com.example.steamstats;
 import com.example.steamstats.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class 	SteamstatsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(SteamstatsApplication.class, args);
 	}
 
