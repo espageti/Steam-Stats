3cad4f9c53f878989eb87dace9ea6947f5f0eddf
espageti <darreeb.lin@gmail.com>
2024-11-26 18:46:32 -0800

    oops

--- steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
+++ steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
@@ -1,6 +1,5 @@
 package com.example.steamstats;
 
-import com.example.steamstats.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
