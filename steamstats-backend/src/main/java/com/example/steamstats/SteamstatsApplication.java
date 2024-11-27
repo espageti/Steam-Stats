36b5d44aa90041df161e6c09be7bdda889bef23c
espageti <darreeb.lin@gmail.com>
2024-11-26 18:31:28 -0800

    change stuff

--- steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
+++ steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
@@ -3,13 +3,11 @@ package com.example.steamstats;
 import com.example.steamstats.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
-public class 	SteamstatsApplication {
+public class SteamstatsApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(SteamstatsApplication.class, args);
 	}
 
