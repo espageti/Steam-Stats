a69b5bf25ca28307eb3c887a232af89b85fbe97d
espageti <darreeb.lin@gmail.com>
2024-11-29 13:35:13 -0800

    add hourly game updates

--- steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
+++ steamstats-backend/src/main/java/com/example/steamstats/SteamstatsApplication.java
@@ -2,14 +2,12 @@ package com.example.steamstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
 public class SteamstatsApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SteamstatsApplication.class, args);
 	}
-
-
-
 }
